bb1c1d8f39b265bf703383ed744ff952ab9e20ea
Deividas Martinkus <147333334+mdeividas19@users.noreply.github.com>
2025-03-10 14:38:37 +0200

    Add car model

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -10,4 +10,9 @@ class Owner extends Model
     use HasFactory;
     protected $fillable = ['name', 'surname', 'phone', 'email', 'address'];
     protected $table = 'owners';
+
+    public function cars()
+    {
+        return $this->hasMany(Car::class);
+    }
 }
