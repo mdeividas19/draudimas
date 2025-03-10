c35083e788e8f6efd035a181a0093e7a5a36fd49
Deividas Martinkus <147333334+mdeividas19@users.noreply.github.com>
2025-03-10 14:47:29 +0200

    Add controller for cars

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Car;
+use App\Models\Owner;
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $cars = Car::all();
+        return view('cars.index', compact('cars'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $owners = Owner::all();
+        return view('cars.create', compact('owners'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $car = new Car();
+        $car->reg_number=$request->reg_number;
+        $car->brand=$request->brand;
+        $car->model=$request->model;
+        $car->owner_id=$request->owner_id;
+        $car->save();
+        return redirect()->route('cars.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Car $car)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Car $car)
+    {
+        $owners = Owner::all();
+        return view('cars.edit', compact('car', 'owners'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Car $car)
+    {
+        $car->reg_number=$request->reg_number;
+        $car->brand=$request->brand;
+        $car->model=$request->model;
+        $car->owner_id=$request->owner_id;
+        $car->save();
+
+        return redirect()->route('cars.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Car $car)
+    {
+        //
+    }
+}
