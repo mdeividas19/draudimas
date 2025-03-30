c1b1b99ea715b9aabcc4d6b47f7c1bc7406a476e
Deividas Martinkus <147333334+mdeividas19@users.noreply.github.com>
2025-03-31 01:16:32 +0300

    Add middleware for checking if user is admin

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,16 @@ use App\Http\Controllers\OwnerController;
 use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\VerifyAuth;
+use App\Http\Middleware\IsAdmin;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::resource('owners', OwnerController::class);
-Route::resource('cars', CarController::class)->middleware(VerifyAuth::class);
+Route::resource('owners', OwnerController::class)->only(['index']);
+Route::resource('owners', OwnerController::class)->except(['index'])->middleware([VerifyAuth::class, IsAdmin::class]);
+Route::resource('cars', CarController::class)->only(['index'])->middleware(VerifyAuth::class);
+Route::resource('cars', CarController::class)->except(['index'])->middleware([VerifyAuth::class, IsAdmin::class]);
 
 Auth::routes();
 
