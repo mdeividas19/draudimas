371b32e2b6f41dc7d4be86515b246581df7c06c6
Deividas Martinkus <147333334+mdeividas19@users.noreply.github.com>
2025-04-28 03:20:20 +0300

    Create policies and gates

---
+++ app/Policies/OwnerPolicy.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Owner;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class OwnerPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Owner $owner): bool
+    {
+        if ($user->type=='admin' || $user->type == 'readonly' || $user->id == $owner->user_id)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Owner $owner): bool
+    {
+        if ($user->type=='admin' || $user->id == $owner->user_id)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Owner $owner): bool
+    {
+        if ($user->type=='admin' || $user->id == $owner->user_id)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Owner $owner): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Owner $owner): bool
+    {
+        return false;
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Owner;
+use App\Models\Car;
+use App\Policies\CarPolicy;
+use App\Policies\OwnerPolicy;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,55 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(Owner::class, OwnerPolicy::class);
+        Gate::policy(Car::class, CarPolicy::class);
+
+        Gate::define('changeLanguage', function ($user) {
+            return true;
+        });
+
+        Gate::define('modifyOwner', function ($user, Owner $owner) {
+            if ($user->id == $owner->user_id) {
+                return true;
+            }
+
+            if ($user->type=='admin') {
+                return true;
+            }
+            return false;
+        });
+
+        Gate::define('modifyCar', function ($user, Car $car) {
+            if ($user->id == $car->owner->user_id) {
+                return true;
+            }
+
+            if ($user->type=='admin') {
+                return true;
+            }
+            return false;
+        });
+
+        Gate::define('viewCar', function ($user, Car $car) {
+            if ($user->id == $car->owner->user_id) {
+                return true;
+            }
+
+            if ($user->type=='admin' || $user->type=='readonly') {
+                return true;
+            }
+            return false;
+        });
+
+        Gate::define('viewOwner', function ($user, Owner $owner) {
+            if ($user->id == $owner->user_id) {
+                return true;
+            }
+
+            if ($user->type=='admin' || $user->type=='readonly') {
+                return true;
+            }
+            return false;
+        });
     }
 }
